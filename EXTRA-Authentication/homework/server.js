9e9a6d1403a24d9c83dea47f0a8c3c25aaa40ca9
Lucia Meyer <luciameyer1603@gmail.com>
2022-07-13 22:23:53 -0300

    hw autenticación 50%

--- EXTRA-Authentication/homework/server.js
+++ EXTRA-Authentication/homework/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieparser = require('cookie-parser');
 
 const app = express();
 
@@ -9,10 +10,50 @@ const users = [
 ]
 
 app.use(morgan('dev'));
+app.use(cookieparser());
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Bienvenidos a Henry!</h1>
+    ${req.cookies.userId ? `
+      <a href='/home'>Perfil</a>
+      <form method='post' action='/logout'>
+        <button>Salir</button>
+      </form>
+      ` : `
+      <a href='/login'>Ingresar</a>
+      <a href='/register'>Registrarse</a>
+      `}
+  `)
+});
+
+app.get('/register', (req, res) => {
+  res.send(`
+    <h1>Registrarse</h1>
+    <form method='post' action='/register'>
+      <input name='name' placeholder='Nombre' required />
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Contraseña' required />
+      <input type='submit' value='Registrarse' />
+    </form>
+    <a href='/login'>Iniciar sesión</a>
+  `)
+});
+
+app.get('/login',  (req, res) => {
+  res.send(`
+    <h1>Iniciar sesión</h1>
+    <form method='post' action='/login'>
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Contraseña' required />
+      <input type='submit' value='Ingresar' />
+    </form>
+    <a href='/register'>Registrarse</a>
   `)
 });
 
